e2f2b0a9ea900090852ade974001a8414f48fdc7
Kimberly Bone <k_bone@uncg.edu>
2019-06-12 15:31:53 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,4 +3,4 @@ puts "What a nutritious meal!"
 puts "A plate of #{protein} with #{veg1} and #{veg2}"
 end
 protein = "meat" 
-meal_choice ("broccoli", "")
+meal_choice ("broccoli", "macaroni", protein)
